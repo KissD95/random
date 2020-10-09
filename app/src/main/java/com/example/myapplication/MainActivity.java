9153f4a352ac227748c6b871b8c673042f8a1ee7
kissd95 <oldtrue69@gmail.com>
2020-10-09 14:30:17 +0200

    Gomb hozzáadva,elhelyezés változtatva,gomb funkció

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    public TextView textDisplay;
+    public Button gomb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        gomb.findViewById(R.id.gomb);
+        textDisplay.findViewById(R.id.textDisplay);
+
+        gomb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textDisplay.setText("Anyádat");
+            }
+        });
     }
 }
